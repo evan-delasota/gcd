c7465ad1304b2ab69597053457293e936691eb65
Evan D <evan.delasota@gmail.com>
2021-06-09 00:55:10 +0000

    GCD solution in Java using Euclid's algorithm. O(log n) runtime.

--- Main.java
+++ Main.java
@@ -2,4 +2,12 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
   }
+
+  public static int gcd(int a, int b) {
+    if (b == 0) {
+      return a;
+    } else {
+      return gcd(b, a % b);
+    }
+  }
 }
